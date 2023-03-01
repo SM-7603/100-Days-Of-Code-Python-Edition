6b5501a1de323258f508fb4b19823317e6541b97
Shubham Mohanty <mohanty.shubham007@gmail.com>
2023-02-28 23:54:37 -0500

    example using the len function and changing data types

---
+++ Day_01/03_len_function.py
@@ -0,0 +1,7 @@
+# using the len() function to "find the length of a string" (the number of charaters that exist in it.)
+
+name = input("Please enter your name: \n")
+
+name_length = str(len(name))
+
+print("The number of letters in your name = " + name_length)
