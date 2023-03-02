5465da4f8eeba7cf8c9296b24059e36148824f3d
Shubham Mohanty <mohanty.shubham007@gmail.com>
2023-03-01 22:46:46 -0500

    improved the solution based on the feedback

--- Day_02/07_Day2_pro3.py
+++ Day_02/07_Day2_pro3.py
@@ -6,29 +6,22 @@
 # Storing 90 in a variable
 death_year = 90
 
-# Calculating the days, weeks and months in 90 years
-death_year_days = death_year * 365
-death_year_weeks = death_year * 52
-death_year_months = death_year * 12
-
 # Asking the current age of the user
 age = input("What is your current age? ")
 
 # Converting age to float
 age_as_float = float(age)
 
-# Calculating the days, weeks and months of the user's current age:
-current_age_in_days = age_as_float * 365
-current_age_in_weeks = age_as_float * 52
-current_age_in_months = age_as_float * 12
+# Calculating age left in years
+age_left = death_year - age_as_float
 
-# Calculating life left in days, weeks, and months:
-life_left_days = death_year_days - current_age_in_days
-life_left_weeks = death_year_weeks - current_age_in_weeks
-life_left_months = death_year_months - current_age_in_months
+# Calculating age left in days, weeks, and months
+age_left_in_days = age_left * 365
+age_left_in_weeks = age_left * 52
+age_left_in_months = age_left * 12
 
 # Results:
-print(f"You have {round(life_left_days)} days, {round(life_left_weeks)} weeks and {round(life_left_months)} months left.")
+print(f"You have {round(age_left_in_days)} days, {round(age_left_in_weeks)} weeks and {round(age_left_in_months)} months left.")
 
 # Feedback:
 # 1. Good job, the output is exactly as expected but the code could be a little more polished as it has some redundant lines of code.
