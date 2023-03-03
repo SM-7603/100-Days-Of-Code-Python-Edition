4d6c872ba7d23c9fa53621f4c0d1fe2c9a905e0c
Shubham Mohanty <mohanty.shubham007@gmail.com>
2023-03-02 22:07:15 -0500

    added the 1st if else program, along with description of comparion operators

---
+++ Day_03/01_conditional_statements.py
@@ -0,0 +1,17 @@
+# Description: Trying out - if else statements
+
+print("Welcome to the rollercoaster!")
+height = int(input("What is your height in cm? "))
+
+if height >= 120:
+    print("You can write the rollercoaster!")
+else:
+    print("Sorry, you have to grow taller before you can ride.")
+
+# Comparison operators
+# >
+# <
+# >=
+# <=
+# == (This isn't the same as "=", the single "=" is used for assignment. Whereas "==" is used for comparing stuff.)
+# !=
