6923a691da96b78096a054430ba454fd520f24ab
Shubham Mohanty <mohanty.shubham007@gmail.com>
2023-03-02 21:03:26 -0500

    added feedback, improved the solution by using string formatting

--- Day_02/08_projectDay2_tip_calculator.py
+++ Day_02/08_projectDay2_tip_calculator.py
@@ -4,21 +4,30 @@
 print("Welcome to the tip calculator!")
 
 # Asking the user for the total bill
-total_bill = input("What was the total bill? $")
+total_bill = float(input("What was the total bill? $"))
 
 # Asking the user for the tip%
-tip_percentage = input(
-    "What percentage tip would you like to give? 10%, 12%, or 15% ? ")
-
-# Calculating the value of tip:
-tip_value = float(tip_percentage) * float(total_bill) / 100
+tip_percentage = int(input(
+    "What percentage tip would you like to give? 10, 12, or 15 ? "))
 
 # Asking for the number of people to split the bill between
-number_of_people = input("How many people to split the bill? ")
+number_of_people = int(input("How many people to split the bill? "))
 
-# Calculating bill paid by each and rounding it off to 2 decimal places
-bill_paid_by_each = round(
-    (float(total_bill) + tip_value) / float(number_of_people), 2)
+# Calculating tip value:
+tip_value = total_bill * tip_percentage / 100
+
+# Calculating bill with tip:
+bill_with_tip = tip_value + total_bill
+
+# Calculating bill paid by each person
+bill_paid_by_each = bill_with_tip / number_of_people
+
+# Formatting the final amount to show 2 decimal places
+bill_paid_by_each = "{:.2f}".format(bill_paid_by_each)
 
 # Result
 print(f"Each person should pay: ${bill_paid_by_each}")
+
+# Feedback:
+# 1. The solution works as intended, but the price should be printed up till 2 decimal places, Try using 150 for amount, 12 for tip and 5 people. The old solution (using round) would give 33.6, instead of 33.60.
+# 2. So this is more of a formatting problem, than a mathematical rounding one. So we'll use try using formatting instead of using math, useing the format function.
