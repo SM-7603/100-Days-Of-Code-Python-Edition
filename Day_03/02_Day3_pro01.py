49049fb03645e61b30f301ec697687acf4f01b60
Shubham Mohanty <mohanty.shubham007@gmail.com>
2023-03-04 00:18:05 -0500

    added the even and odd checker problem

---
+++ Day_03/02_Day3_pro01.py
@@ -0,0 +1,8 @@
+# Program Description: Odd or Even checker
+
+number = int(input("Enter any number: "))
+
+if number % 2 == 0:
+    print(f"{number} is as even")
+else:
+    print(f"{number} is an odd")
