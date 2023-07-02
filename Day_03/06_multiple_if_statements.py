cbe35c47875f5b8ecfa9bb0b7cb82f5e7c40ad24
Shubham Mohanty <mohanty.shubham007@gmail.com>
2023-07-02 04:04:07 -0400

    add attempt 2 at solving the problem, make a few changes in the indentation & and the structure of the if statements

--- Day_03/06_multiple_if_statements.py
+++ Day_03/06_multiple_if_statements.py
@@ -1,31 +1,29 @@
 # Description: Borrowing the code that I wrote for the roller coaster program, and then adding successive if-statements :D
 
 # Declare variables
-ticket_price_kid = 5
-ticket_price_teen = 7
-ticket_price_adult = 12
+bill = 0
 
 print("Welcome to the rollercoaster!")
 height = int(input("What is your height in cm? "))
-want_photo = (input("Do you want to take a picture? (y / n) "))
 
 if height >= 120:
     print("You can ride the rollercoaster!")
     age = int(input("What is your age? "))
     if age < 12:
-        if want_photo == "y":
-            print(f"Please pay ${ticket_price_kid + 3}")
-        else:
-            print(f"Please pay ${ticket_price_kid}")
+        bill = 5
+        print(f"Please pay ${bill}")
     elif age <= 18:
-        if want_photo == "y":
-            print(f"Please pay ${ticket_price_teen + 3}")
-        else:
-            print(f"Please pay ${ticket_price_teen}")
+        bill = 7
+        print(f"Please pay ${bill}")
     else:
-        if want_photo == "y":
-            print(f"Please pay ${ticket_price_adult + 3}")
-        else:
-            print(f"Please pay ${ticket_price_adult}")
+        bill = 12
+        print(f"Please pay ${bill}")
+    
+    want_photo = (input("Do you want to take a picture? (y / n) "))
+    if want_photo == "y":
+        bill += 3 # as the bill increments by $3 for the ticket
+    
+    print(f"Please pay ${bill}")
+
 else:
     print("Sorry, you have to grow taller before you can ride :D")
