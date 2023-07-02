805e6e9bb43893f80a146610daf4b887d7987dc0
Shubham Mohanty <mohanty.shubham007@gmail.com>
2023-07-02 03:20:28 -0400

    add actual code solution for the leap year problem

--- Day_03/05_Day3_pro03.py
+++ Day_03/05_Day3_pro03.py
@@ -20,3 +20,15 @@
 # else:
 #     output "no"
 
+year = int(input("Enter the year: "))
+
+if year % 4 == 0:
+    if year % 100 == 0:
+        if year % 400 == 0:
+            print(f"{year} is a leap year :D")
+        else:
+            print(f"{year} is not a leap year :(")
+    else:
+        print(f"{year} is a leap year :D")
+else:
+    print(f"{year} is not a leap year :(")
