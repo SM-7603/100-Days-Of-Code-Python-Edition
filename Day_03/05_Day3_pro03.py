037c6234410064de85a081edc8a1d96d77954a82
Shubham Mohanty <mohanty.shubham007@gmail.com>
2023-07-02 03:19:46 -0400

    add leap year conditions, and the pseudoCode

--- Day_03/05_Day3_pro03.py
+++ Day_03/05_Day3_pro03.py
@@ -1,1 +1,22 @@
- 
+# Warning - This is a difficult challenge (could take anywhere from 10-40 mins)
+# Tip: Try drawing out the flow chart and writing out the pseudoCode for this problem on a piece of paper, it would be good practice... :D 
+
+# Leap year conditions: (For every year thats:)
+# 1. Perfectly divisible by 4
+#   -  Except when its perfectly divisible by 100
+#       - Unless its perfectly divisible by 400
+
+# PseudoCode:
+# start
+# input year
+# if year % 4 == 0:
+#     if year % 100 == 0:
+#         if year % 400 == 0:
+#             output "yes"
+#         else:
+#             output "no"
+#     else:
+#         output "yes"
+# else:
+#     output "no"
+
