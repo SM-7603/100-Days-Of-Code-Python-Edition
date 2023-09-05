483b7d868e6a72a48ca7e870c196e1a371f752ae
Shubham Mohanty <mohanty.shubham007@gmail.com>
2023-09-04 21:00:42 -0400

    feat(prices): Define pizza price & topping prices
    add the pricing info as metadata

--- Day_03/07_Day3_pro04.py
+++ Day_03/07_Day3_pro04.py
@@ -2,6 +2,23 @@
 
 # Based on the user's input, we need to work out the customer's final bill
 
+""" 
+Meta Data:
+
+- Pizza Prices: 
+    - S = $15
+    - M = $20
+    - L = $25
+
+- Pepperoni:
+    -  S  = +$2
+    - M/L = +$3
+
+- Extra Cheese:
+    - Any size = +$1
+
+"""
+
 # Here's the starter code:
 
 print("Welcome to Python Pizza Deliveries!")
