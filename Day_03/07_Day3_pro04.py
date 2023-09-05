32c2b9e44a0573e7f0d0100365c946a719a31d83
Shubham Mohanty <mohanty.shubham007@gmail.com>
2023-09-04 20:58:47 -0400

    chore(init): Start building the pizza order program
    Set up the initial code for the pizza order program. It includes input prompts for pizza size, pepperoni preference, and extra cheese.

---
+++ Day_03/07_Day3_pro04.py
@@ -0,0 +1,11 @@
+# Task: We're building a pizza order program here
+
+# Based on the user's input, we need to work out the customer's final bill
+
+# Here's the starter code:
+
+print("Welcome to Python Pizza Deliveries!")
+size = input("What size pizza do you want? S, M or L: ")
+add_pepperoni = input("Do you want pepperoni? Y or N: ")
+extra_cheese = input("Do you want extra cheese? Y or N: ")
+
